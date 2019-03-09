b1dfd824c9b26c56ac0c868350639df3d0b8a2a7
Eugene <eugenesny@icloud.com>
2019-03-10 00:01:54 +0200

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("efdldkhneasjdwabn");
+            Console.Read();
+        }
+    }
+}
